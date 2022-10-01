c4b31f94b896e5fbef8467ce1f744a14f3bce760
Paul <paulr@freelearn.tv>
2022-10-02 10:26:31 +1300

    added assets folders

---
+++ lib/src/services/http_service.dart
@@ -0,0 +1,38 @@
+// ignore_for_file: non_constant_identifier_names, no_leading_underscores_for_local_identifiers, body_might_complete_normally_nullable, avoid_print
+
+import '../models/app_config.dart';
+
+//Packages
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+
+class HTTPService {
+  final Dio dio = Dio();
+  final GetIt getIt = GetIt.instance;
+
+  String? _base_url;
+  String? _api_key;
+
+  HTTPService() {
+    AppConfig _config = getIt.get<AppConfig>();
+    _base_url = _config.BASE_API_URL;
+    _api_key = _config.API_KEY;
+  }
+
+  Future<Response?> get(String _path, {Map<String, dynamic>? query}) async {
+    try {
+      String _url = '$_base_url$_path';
+      Map<String, dynamic> _query = {
+        'api_key': _api_key,
+        'language': 'en-US',
+      };
+      if (query != null) {
+        _query.addAll(query);
+      }
+      return await dio.get(_url, queryParameters: _query);
+    } on DioError catch (e) {
+      print('Unable to perform get request.');
+      print('DioError:$e');
+    }
+  }
+}
